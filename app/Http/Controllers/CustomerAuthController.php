16d0bafaef12ddc9fb82eee46a39e1dd2e10ace5
brk767121 <bappypust@gmail.com>
2023-11-30 00:11:28 +0600

    Create Customer Model, Migration, Api, Cors Setup, And Modify Config for Customer Auth

---
+++ app/Http/Controllers/CustomerAuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+
+class CustomerAuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required',
+            'mobile' => 'required',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $customer = Customer::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'mobile' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        // You might also generate and return a JWT token here
+
+        return response()->json(['message' => 'Registration successful']);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            // Authentication passed
+            return response()->json(['message' => 'Login successful']);
+        }
+
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+}
